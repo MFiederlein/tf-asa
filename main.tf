9ac57099e2eff79264389ce556f73c5e7872247f
Matt Fiederlein <22824972+MFiederlein@users.noreply.github.com>
2023-02-04 21:13:05 -0500

    init

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    ciscoasa = {
+      source  = "CiscoDevNet/ciscoasa"
+      version = "1.3.0"
+    }
+  }
+}
+
+provider "ciscoasa" {
+}
+
+resource "ciscoasa_network_object" "ipv4host" {
+  name  = "ipv4host"
+  value = "192.168.10.5"
+}
